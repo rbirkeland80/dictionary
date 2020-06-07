08020f61cfb9c9fef456dad4a214915a6ad6d14f
Marta <marta@mtupikina-MBP.local>
2020-06-07 15:26:36 +0300

    add /quiz endpoint; refactor

--- routes/api/words/index.js
+++ routes/api/words/index.js
@@ -2,17 +2,78 @@ const routes = require('express').Router();
 const Word = require('./model.js');
 const BaseCrud = require('../common/index.js');
 
-const WordCrud = new BaseCrud(Word);
+const QUIZ_TYPE_NEWEST = 'newest';
+const QUIZ_TYPE_OLDEST = 'oldest';
+const QUIZ_TYPE_RANDOM = 'random';
+
+class WordCrud extends BaseCrud {
+  constructor(Word) {
+    super(Word);
+  }
+
+  async generateQuiz(req, res) {
+    const {
+      includeToVerify,
+      maxCount = 50,
+      type = QUIZ_TYPE_NEWEST,
+      limit = 50,
+      skip = 0,
+      fields
+    } = req.body;
+    const project = {};
+    const createdAtDirection = type === QUIZ_TYPE_NEWEST ? -1 : 1;
+    fields.forEach(prop => project[prop] = 1);
+
+    const includeToVerifySet = includeToVerify
+      ? []
+      : [{ $match: { toVerifyNextTime: { $ne: true } } }];
+    const randomSet = type === QUIZ_TYPE_RANDOM
+      ? [{ $sample: { size: maxCount } }]
+      : [];
+    const sortSet = type === QUIZ_TYPE_NEWEST || type === QUIZ_TYPE_OLDEST
+      ? [{ $sort: { createdAt : createdAtDirection } }]
+      : [];
+
+    try {
+      const totalCount = await this.model.aggregate([
+        ...includeToVerifySet,
+        { $count: 'count' }
+      ]);
+
+      const count = totalCount <= maxCount ? totalCount : maxCount;
+
+      const data = await this.model.aggregate([
+        ...includeToVerifySet,
+        ...randomSet,
+        ...sortSet,
+        { $limit : limit > count ? count : limit },
+        { $skip : skip },
+        { $project : project }
+      ]);
+
+      console.log(data);
+
+      res.json({ list: data, count, limit, skip });
+    } catch(error) {
+      this.handleErrorResponse(error, res);
+    }
+  }
+}
+
+const wordCrud = new WordCrud(Word);
 
 routes.route('/')
-  .get((req, res) => WordCrud.getAllEntries(req, res))
-  .post((req, res) => WordCrud.saveNewEntry(req, res));
+  .get((req, res) => wordCrud.getAllEntries(req, res))
+  .post((req, res) => wordCrud.saveNewEntry(req, res));
 
-routes.param('id', (req, res, next, id) => WordCrud.checkIdParam(req, res, next, id));
+routes.route('/quiz')
+  .post((req, res) => wordCrud.generateQuiz(req, res));
+
+routes.param('id', (req, res, next, id) => wordCrud.checkIdParam(req, res, next, id));
 
 routes.route('/:id')
-  .get((req, res) => WordCrud.getEntryById(req, res))
-  .put((req, res) => WordCrud.updateEntryById(req, res))
-  .delete((req, res) => WordCrud.deleteEntryById(req, res));
+  .get((req, res) => wordCrud.getEntryById(req, res))
+  .put((req, res) => wordCrud.updateEntryById(req, res))
+  .delete((req, res) => wordCrud.deleteEntryById(req, res));
 
 module.exports = routes;
