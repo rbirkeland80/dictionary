1b1ebf7a0fae11b45e2b82a510878f164108bba7
Marta <marta@mtupikina-MBP.local>
2020-12-16 14:52:07 +0200

    update resp text

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 const words = require('./words');
 
-routes.get('/', (req, res) => res.send('The api node!'));
+routes.get('/', (req, res) => res.send('SUCCESS: The api node!'));
 
 routes.use('/words', words);
 
