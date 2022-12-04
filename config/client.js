d1a384f3271d8dc43426b767d1b76c084b8af61e
rbirkeland80 <34028289+rbirkeland80@users.noreply.github.com>
2022-12-04 17:11:39 +0200

    Update client.js

--- config/client.js
+++ config/client.js
@@ -1,4 +1,4 @@
 module.exports = {
-  PROD: 'https://e-dictionary-nsylo702b-rbirkeland80.vercel.app/',
+  PROD: 'https://e-dictionary-nsylo702b-rbirkeland80.vercel.app/', // test comment
   LOCAL: 'http://localhost:3000'
 };
